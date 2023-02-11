1c6b102b506c04dcfc7a7747c916b50f9a2aa576
Oddward <mugtaba.gar@gmail.com>
2023-02-11 16:16:16 +0200

    initial custom commit for metadata & post

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'The Oddword blog',
+    site_url: 'https://oddword.vercel.app',
+    feed_url: 'https://oddword.vercel.app/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
